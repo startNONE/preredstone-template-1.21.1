813bea7faa418c8f1e54a350960fb18d927aeb99
startNONE <2065157374@qq.com>
2025-05-01 11:22:44 +0800

    first build

--- src/main/java/com/preredstone/PreRedStone.java
+++ src/main/java/com/preredstone/PreRedStone.java
@@ -1,5 +1,6 @@
 package com.preredstone;
 
+import com.preredstone.items.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -18,7 +19,7 @@ public class PreRedStone implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		ModItems.registerModItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
--- src/main/java/com/preredstone/items/ModItems.java
+++ src/main/java/com/preredstone/items/ModItems.java
@@ -4,13 +4,15 @@ import com.preredstone.PreRedStone;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public  static  final  Item PRE_REDSTONE
+    public  static  final  Item PRE_REDSTONE = registerItems("pre_redstone",new Item(new Item.Settings()));
     private static Item registerItems(String id, Item item){
  //       return Registry.register(Registries.ITEM,RegistryKey.of(Registries.ITEM.getKey(), Identifier.of(PreRedStone.MOD_ID, id), item);
-        return Registry.register(Registries.ITEM,RegistryKey.of(Registries.ITEM.getKey(), Identifier.of(PreRedStone.MOD_ID, id), item);
+        return Registry.register(Registries.ITEM, Identifier.of(PreRedStone.MOD_ID, id), item);
+    }
+    public  static void registerModItems(){
+        PreRedStone.LOGGER.info("Registering Items");
     }
 }
